1c3e7669f8a22aba1ed4ef284c581725af6316f0
penghui <penghui.zhang@foxmail.com>
2018-01-19 11:14:30 +0800

    prettier config

---
+++ snippets/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babylon',
+  printWidth: 80,
+  // default is true
+  semi: false,
+  // default is false
+  singleQuote: true,
+  tabWidth: 2,
+  // default is 'none'
+  trailingComma: 'all',
+  useTabs: false,
+}
