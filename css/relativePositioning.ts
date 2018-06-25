044ac1a198e6c57cd1cc8ffa10dd777b78711535
penghui <pzhang2@xiaohongshu.com>
2018-06-25 10:10:45 +0800

    doc(css): resolve display, float, position

--- css/relativePositioning.ts
+++ css/relativePositioning.ts
@@ -7,6 +7,7 @@ interface CssDeclarations {
   position: 'relative',
 }
 
+// refer to https://www.w3.org/TR/CSS21/visuren.html#relative-positioning
 function resolveRelativePositioning(options: CssDeclarations) {
   const computedOptions = { ...options }
 
@@ -14,12 +15,23 @@ function resolveRelativePositioning(options: CssDeclarations) {
     computedOptions.left = '0'
     computedOptions.right = '0'
   } else if (computedOptions.left === 'auto') {
-    computedOptions.right = `-${computedOptions.left}`
-  } else if (computedOptions.right === 'auto') {
     computedOptions.left = `-${computedOptions.right}`
+  } else if (computedOptions.right === 'auto') {
+    computedOptions.right = `-${computedOptions.left}`
   } else if (options.direction === 'ltr') {
-    computedOptions.right = 'ignored'
+    computedOptions.right = `-${options.left}`
   } else if (options.direction === 'rtl') {
-    computedOptions.left === 'ignored'
+    computedOptions.left === `-${options.right}`
+  }
+
+  if (computedOptions.top === 'auto' && computedOptions.bottom === 'auto') {
+    computedOptions.top = '0'
+    computedOptions.bottom = '0'
+  } else if (computedOptions.top === 'auto') {
+    computedOptions.top = `-${computedOptions.bottom}`
+  } else if (computedOptions.bottom === 'auto') {
+    computedOptions.bottom = `-${computedOptions.top}`
+  } else {
+    computedOptions.bottom = `-${options.top}`
   }
 }
